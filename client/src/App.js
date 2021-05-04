d90d24e6bb8b38ca6438f82f954f817d6000fe2d
Olli1982 <granatenwerfer@web.de>
2021-05-04 12:15:14 +0200

    Einrichten des Frontends Teil1

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
-function App() {
+import { listLogEntries } from './API';
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 53.0,
+    longitude: 9.0,
+    zoom: 3,
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      mapStyle="mapbox://styles/azzu/cko9tbml44mwd17nxzii13bxm"
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {logEntries.map((entry) => (
+        <Marker
+          key={entry._id}
+          latitude={entry.latitude}
+          longitude={entry.longitude}
+          offsetLeft={-12}
+          offsetTop={-24}
         >
-          Learn React now!
-        </a>
-      </header>
-    </div>
+          <svg
+            className="marker"
+            style={{
+              width: '30px',
+              height: '30px',
+            }}
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            title="RoomSharp"
+            data-ga-event-category="material-icons"
+            data-ga-event-action="click"
+            data-ga-event-label="RoomSharp"
+          >
+            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"></path>
+          </svg>
+        </Marker>
+      ))}
+    </ReactMapGL>
   );
-}
+};
 
 export default App;
